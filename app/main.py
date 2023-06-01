6c92e9d622de98a777ddae42515aa99a01d33e75
Yash Chauhan <y4shchauhan@gmail.com>
2023-06-01 19:33:27 +0530

    basic config and create user model

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from cassandra.cqlengine.management import sync_table
+from fastapi import FastAPI
+
+from . import db
+from .users.models import User
+
+app = FastAPI()
+DB_SESSION = None
+
+
+@app.on_event("startup")
+def on_startup():
+    global DB_SESSION
+    DB_SESSION = db.get_session()
+    sync_table(User)
+
+
+@app.get('/users')
+def users_list_view():
+    q = User.objects.all().limit(10)
+    return list(q)
